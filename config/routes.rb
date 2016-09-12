29b4e8c40657a487065448abb12b02baf0050300
Brian Sayler <sayler.b@gmail.com>
2016-09-12 13:22:17 -0600

    add api route for link creation and create method

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :links, only: [:index]
+      resources :links, only: [:index, :create]
     end
   end
 end
