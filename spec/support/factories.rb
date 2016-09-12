8ff9e6280ead9731259ed34a5657aff5d254db33
Brian Sayler <sayler.b@gmail.com>
2016-09-12 11:25:39 -0600

    add link factory

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -6,4 +6,12 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
   end
+
+  sequence(:title) { |n| "#{n} Link Title" }
+
+  factory :link do
+    title 
+    url "https://www.turing.io/"
+    user
+  end
 end
