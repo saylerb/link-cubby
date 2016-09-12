2f14bcb5e33bc2620937757fae34fad30a7ef499
Brian Sayler <sayler.b@gmail.com>
2016-09-12 09:41:53 -0600

    configure factory girl and shoulda matchers
    - prevented factory generators
    - allow factory girl syntax
    - add a factory for user
    - adjusted model spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user, password: nil, password_confirmation: nil) }
+  let(:user1) { build(:user) }
+  let(:user2) { build(:user, password: nil, password_confirmation: nil) }
   
   it "has a valid factory" do
-    expect(user).to be_valid
+    expect(user1).to be_valid
   end
 
   context "responds to the correct attributes" do
-    it { expect(user).to respond_to(:email) }
-    it { expect(user).to respond_to(:password) }
+    it { expect(user1).to respond_to(:email) }
+    it { expect(user1).to respond_to(:password) }
   end
 
   context "validations" do
-    it { expect(user_without_password).to validate_presence_of(:email) }
-    it { expect(user_without_password).to validate_uniqueness_of(:email) }
-    it { expect(user_without_password).to validate_presence_of(:password) }
-    it { expect(user_without_password).to validate_confirmation_of(:password) }
+    it { expect(user2).to validate_presence_of(:email) }
+    it { expect(user2).to validate_uniqueness_of(:email) }
+    it { expect(user2).to validate_presence_of(:password) }
+    it { expect(user2).to validate_confirmation_of(:password) }
   end
 end
