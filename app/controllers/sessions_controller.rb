ae2ba555aef89dee51d6040ede37077b0fe53a07
Brian Sayler <sayler.b@gmail.com>
2016-09-12 10:48:01 -0600

    complete session creation
    - added a post route
    - add session creation method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully Logged In!"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Invalid Credentials"
+      render :new
+    end
+  end
 end
