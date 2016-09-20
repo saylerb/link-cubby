36d485eed43e11a0d5c72d11204359657c4e6b81
Brian Sayler <sayler.b@gmail.com>
2016-09-20 17:32:47 -0600

    add model spec for sorting

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Link, type: :model do
       [link1, link2, link3].each { |link| expect(link).to be_invalid }
     end
 
-    it 'should reject non-URL strings' do
+    it "should reject non-URL strings" do
       link1 = build(:link, url: 'NON-URL-STRING')
       link2 = build(:link, url: 'nonurlstring')
       link3 = build(:link, url: 'http://')
@@ -51,9 +51,51 @@ RSpec.describe Link, type: :model do
       [link1, link2, link3].each { |link| expect(link).to be_invalid }
     end
 
-    it 'should reject a blank URL' do
+    it "should reject a blank URL" do
       link = build(:link, url: '')
       expect(link).to be_invalid
+    end
+  end
+
+  describe "sorts URLs using params" do
+    it "should sort URLS desc by title" do
+      user = create(:user)
+      link1 = create(:link, title: "AAA", url: 'http://www.google.com', user: user)
+      link2 = create(:link, title: "ZZZ", url: 'https://www.reddit.com', user: user)
+      link3 = create(:link, title: "MMM", url: 'https://www.stackoverflow.com', user: user)
+
+      expect(Link.sort_by_params(user, 'title', 'desc').first).to eq(link2)
+      expect(Link.sort_by_params(user, 'title', 'desc').last).to eq(link1)
+    end
+
+    it "should sort URLS asc by title" do
+      user = create(:user)
+      link1 = create(:link, title: "AAA", url: 'http://www.google.com', user: user)
+      link2 = create(:link, title: "ZZZ", url: 'https://www.reddit.com', user: user)
+      link3 = create(:link, title: "MMM", url: 'https://www.stackoverflow.com', user: user)
+
+      expect(Link.sort_by_params(user, 'title', 'asc').first).to eq(link1)
+      expect(Link.sort_by_params(user, 'title', 'asc').last).to eq(link2)
+    end
+
+    it "should sort URLS desc by url" do
+      user = create(:user)
+      link1 = create(:link, title: "AAA", url: 'http://www.google.com', user: user)
+      link2 = create(:link, title: "ZZZ", url: 'https://www.reddit.com', user: user)
+      link3 = create(:link, title: "MMM", url: 'https://www.stackoverflow.com', user: user)
+
+      expect(Link.sort_by_params(user, 'url', 'desc').first).to eq(link3)
+      expect(Link.sort_by_params(user, 'url', 'desc').last).to eq(link1)
+    end
+
+    it "should sort URLS asc by url" do
+      user = create(:user)
+      link1 = create(:link, title: "AAA", url: 'http://www.google.com', user: user)
+      link2 = create(:link, title: "ZZZ", url: 'https://www.reddit.com', user: user)
+      link3 = create(:link, title: "MMM", url: 'https://www.stackoverflow.com', user: user)
+
+      expect(Link.sort_by_params(user, 'url', 'asc').first).to eq(link1)
+      expect(Link.sort_by_params(user, 'url', 'asc').last).to eq(link3)
     end
   end
 end
