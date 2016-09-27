c82b28bb06c8bfaae3f83a3c4a45e9eba40cc21e
Brian Sayler <sayler.b@gmail.com>
2016-09-26 20:40:02 -0600

    add spec to ensure error message removed on update
    - move update status scenarios to separate spec
    - move all attribute update scenarios to own spec

--- spec/features/user_updates_link_attr_spec.rb
+++ spec/features/user_updates_link_attr_spec.rb
@@ -1,31 +1,6 @@
 require "rails_helper"
 
 RSpec.feature "update link attributes", js: true do
-  scenario "user can click a button to update status" do
-    user = create_and_authenticate_user
-    link = create(:link, url: "https://www.google.com/", user: user)
-
-    visit root_path
-
-    within "#read-#{link.id}" do
-      expect(page).to have_content("false")
-    end
-
-    within "#update-status-#{link.id}" do
-      expect(page).to have_content("Mark as Read")
-
-      click_on "Mark as Read"
-    end
-
-    within "#read-#{link.id}" do
-      expect(page).to have_content("true")
-    end
-
-    within "#update-status-#{link.id}" do
-      expect(page).to have_content("Mark as Unread")
-    end
-  end
-
   context "with valid input" do
     scenario "user can update link title" do
       user = create_and_authenticate_user
@@ -60,6 +35,7 @@ RSpec.feature "update link attributes", js: true do
       within "#link-#{link.id}" do
         expect(page).to have_content("https://www.google.com/")
 
+        page.execute_script("$('#url-#{link.id}').focus()")
         page.execute_script("$('#url-#{link.id}').html('https://www.stackoverflow.com')")
         page.execute_script("$('#url-#{link.id}').blur()")
 
@@ -69,6 +45,34 @@ RSpec.feature "update link attributes", js: true do
 
       expect(page).to have_content("Updated successfully!")
       expect(page).to have_content("https://www.stackoverflow.com")
+      expect(Link.find(link.id).url).to eq("https://www.stackoverflow.com")
+    end
+    
+    scenario "error message should clear after entering a valid update" do
+      user = create_and_authenticate_user
+      link = create(:link, title: "original title", url: "https://www.google.com/", user: user)
+
+      visit root_path
+
+      expect(Link.find(link.id).title).to eq("original title")
+
+      page.execute_script("$('#table-body').prepend(\"<tr class='bg-danger'><td colspan='5'>Error message</td></tr>\")")
+
+      within "tr.bg-danger" do
+        expect(page).to have_content("Error message")
+      end
+
+      within "#link-#{link.id}" do
+        expect(page).to have_content("https://www.google.com/")
+
+        page.execute_script("$('#url-#{link.id}').focus()")
+        page.execute_script("$('#url-#{link.id}').html('https://www.stackoverflow.com')")
+        page.execute_script("$('#url-#{link.id}').blur()")
+      end
+
+      expect(page).to_not have_content("Error message")
+      expect(page).to_not have_css("tr.bg-danger")
+      expect(page).to_not have_content("Updated Successfully!")
       expect(Link.find(link.id).url).to eq("https://www.stackoverflow.com")
     end
   end
@@ -86,7 +90,7 @@ RSpec.feature "update link attributes", js: true do
         expect(page).to have_content("https://www.google.com/")
 
         page.execute_script("$('#title-#{link.id}').focus()")
-        page.execute_script("$('#title-#{link.id}').html('')") # try to delete title
+        page.execute_script("$('#title-#{link.id}').html('')")
         page.execute_script("$('#title-#{link.id}').blur()")
       end
 
@@ -121,3 +125,4 @@ RSpec.feature "update link attributes", js: true do
     end
   end
 end
+
